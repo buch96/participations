8899d931fcba9bdaeb4c1ec2a8f5eab39cf88bfe
buch96 <46726893+buch96@users.noreply.github.com>
2019-02-07 17:22:38 -0600

    Add files via upload

---
+++ Participation1/Participation1/EntryForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation1
+{
+    class EntryForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+        public EntryForm()
+        { Name = string.Empty;
+            Address = string.Empty;
+            ZipCode = 0;
+        }
+        public EntryForm(string name, string address, int zipcode)
+        { Name = name;
+            Address = address;
+            ZipCode = zipcode;
+        }
+        public override string ToString()
+        {
+            return Name + ", " + Address + ", " + ZipCode;
+        }
+    }
+}
---
+++ Participation1/Participation1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿//blake buchanan here, if you're expecting me to remember my name this semester you're gonna have a bad time
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Participation1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterFormButton_Click(object sender, RoutedEventArgs e)
+        {
+            EntryForm ee = new EntryForm(NameTextBox.Text, AddressBox.Text, Convert.ToInt32(ZipBox.Text));
+            BigYellowListBox.Items.Add(ee);
+        }
+    }
+}
